b347510274623a5bf8a6a92dd493d48294700fd0
sihadan <def6488@gmail.com>
2017-11-21 23:27:49 +0900

    add tower

---
+++ hueTD/Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager gm = null;
+
+	void Awake () {
+
+        /*
+         * GameManager initialize
+         */
+
+        // GameManager must be singleton object
+        if (gm == null)
+            gm = this;
+        else
+            Destroy(this.gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ hueTD/Assets/Scripts/Tower.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour {
+
+    private GameManager gm;
+
+    public int towerLevel;
+    public string towerName;
+    public string essence;
+    public Sprite icon;
+
+    public int damage;
+    public int speed;
+    public int range;
+    public int size;
+    public int price;
+    // private towerEffect effect;
+
+    private float shotInterval;
+
+	void Start () {
+        gm = GameManager.gm;
+        shotInterval = 1 / speed;
+	}
+
+    void Update () {
+		
+	}
+}
